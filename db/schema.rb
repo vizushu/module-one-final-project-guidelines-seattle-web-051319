88e4fd6dc35116298ef72a3d0e8e908e25e3f0d4
github username <github email address>
2019-05-29 13:40:08 -0700

    added dislike table and class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190528230640) do
+ActiveRecord::Schema.define(version: 20190529202617) do
+
+  create_table "dislikes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "song_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,6 @@
 class Song < ActiveRecord::Base
   has_many :likes
   has_many :users, through: :likes
+  has_many :dislikes
+  has_many :users, through: :dislikes
 end
