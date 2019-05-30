3d2ae83d9cbfec9876512eaf8fdab1648dd0a802
AustinBH <harlowaustin2@gmail.com>
2019-05-30 11:14:52 -0700

    added while loop to prevent duplication

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,13 @@
 require_relative '../config/environment'
 
 def get_songs
-    songs = RestClient.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&page=1&api_key=#{API_KEY}&format=json")
+  counter = 0
+  while counter < 10
+    songs = RestClient.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&page=#{counter+1}&api_key=#{API_KEY}&format=json")
     parsed_songs = JSON.parse(songs)
-end
-
-def get_songs_2
-    songs = RestClient.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&page=2&api_key=#{API_KEY}&format=json")
-    parsed_songs = JSON.parse(songs)
-end
-
-def get_songs_3
-    songs = RestClient.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&page=3&api_key=#{API_KEY}&format=json")
-    parsed_songs = JSON.parse(songs)
-end
-
-def get_songs_4
-    songs = RestClient.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&page=4&api_key=#{API_KEY}&format=json")
-    parsed_songs = JSON.parse(songs)
-end
-
-def get_songs_5
-    songs = RestClient.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&page=5&api_key=#{API_KEY}&format=json")
-    parsed_songs = JSON.parse(songs)
+    create_songs(parsed_songs)
+    counter += 1
+  end
 end
 
 def create_songs(song_data)
@@ -31,8 +16,4 @@ def create_songs(song_data)
   end
 end
 
-create_songs(get_songs)
-create_songs(get_songs_2)
-create_songs(get_songs_3)
-create_songs(get_songs_4)
-create_songs(get_songs_5)
+get_songs
