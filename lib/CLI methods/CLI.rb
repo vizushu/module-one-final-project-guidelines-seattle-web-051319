e4134ec81a2a278a11e0757dae7bf16d832ee787
AustinBH <harlowaustin2@gmail.com>
2019-05-30 23:13:53 -0700

    added liked_disliked_songs to CLIMethods to clear main CLI.rb file

--- lib/CLI methods/CLI.rb
+++ lib/CLI methods/CLI.rb
@@ -216,30 +216,12 @@ class CLI
     def all_liked_disliked_songs(indicator)
       if indicator == 1
         @preference = 1
-        liked_disliked_songs(1)
+        CLIMethods.liked_disliked_songs(1)
       elsif indicator == 2
         @preference = 2
-        liked_disliked_songs(2)
+        CLIMethods.liked_disliked_songs(2)
       end
       likes_dislikes_menu
-    end
-
-    #This method is called upon in all_liked_disliked_songs and displays all of
-    #a user's liked songs
-    def liked_disliked_songs(preference)
-      if preference == 1
-        CLIOutputs.liked_songs_title
-        @@user_1.likes.each_with_index do |like, index|
-          puts "#{index+1}. #{like.song.name} by: #{like.song.artist.name}"
-        end
-        CLIOutputs.likes_menu_options
-      elsif preference == 2
-        CLIOutputs.disliked_songs_title
-        @@user_1.dislikes.each_with_index do |dislike, index|
-          puts "#{index+1}. #{dislike.song.name} by: #{dislike.song.artist.name}"
-        end
-        CLIOutputs.dislikes_menu_options
-      end
     end
 
     #This method is used to give a user the update, delete, go back, and exit
--- lib/CLI methods/CLIMethods.rb
+++ lib/CLI methods/CLIMethods.rb
@@ -10,4 +10,22 @@ class CLIMethods
     end
   end
 
+  #This method is called upon in all_liked_disliked_songs and displays all of
+  #a user's liked songs
+  def self.liked_disliked_songs(preference)
+    if preference == 1
+      CLIOutputs.liked_songs_title
+      CLI.user_1.likes.each_with_index do |like, index|
+        puts "#{index+1}. #{like.song.name} by: #{like.song.artist.name}"
+      end
+      CLIOutputs.likes_menu_options
+    elsif preference == 2
+      CLIOutputs.disliked_songs_title
+      CLI.user_1.dislikes.each_with_index do |dislike, index|
+        puts "#{index+1}. #{dislike.song.name} by: #{dislike.song.artist.name}"
+      end
+      CLIOutputs.dislikes_menu_options
+    end
+  end
+
 end
